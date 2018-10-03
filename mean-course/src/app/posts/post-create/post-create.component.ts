ad87508acdff062164c1ca143e1461312d55fc2d
Eloy Hernández <eloyhz@gmail.com>
2018-10-03 14:15:33 -0500

    Two-way binding

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -5,12 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredValue = '';
   newPost = 'NO CONTENT';
 
-  onAddPost(postInput: HTMLTextAreaElement) {
-    console.log(postInput);
-    console.dir(postInput);
-    this.newPost = postInput.value;
-   // alert('Post added');
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 }
